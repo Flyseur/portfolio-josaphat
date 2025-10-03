ea9f030fedeaaf3576450ac646786843995e0404
Josaphat Boua <josaphatboua220@gmail.com>
2025-10-03 11:50:16 +0000

    Try fixing this error: `Console Error: ReactDOM.useFormState has been re

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useEffect } from "react";
-import { useFormState, useFormStatus } from "react-dom";
+import { useEffect, useActionState } from "react";
+import { useFormStatus } from "react-dom";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
@@ -47,7 +47,7 @@ function SubmitButton() {
 
 export function ContactSection() {
   const { toast } = useToast();
-  const [state, formAction] = useFormState(submitContactForm, initialState);
+  const [state, formAction] = useActionState(submitContactForm, initialState);
   const { register, reset, formState: { errors } } = useForm<ContactFormValues>({
     resolver: zodResolver(contactSchema),
   });
