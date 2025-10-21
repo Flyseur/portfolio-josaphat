f8c6bb62b48fbc20bf3a44b92ac69a8ec3972377
Josaphat Boua <josaphatboua220@gmail.com>
2025-10-21 12:45:56 +0000

    https://i.postimg.cc/HWZYZZwB/icon.png

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ export const metadata: Metadata = {
   title: 'Boua Josaphat Olivier Konan - Développeur Full Stack',
   description: 'Portfolio de Boua Josaphat Olivier Konan, un développeur full stack créatif.',
   icons: {
-    icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚙️</text></svg>',
+    icon: 'https://i.postimg.cc/HWZYZZwB/icon.png',
   },
 };
 
