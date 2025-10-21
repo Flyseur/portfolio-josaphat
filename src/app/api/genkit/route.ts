61f32e11935b56afafe26d99bb0a98e4ea916a71
Josaphat Boua <josaphatboua220@gmail.com>
2025-10-21 11:48:26 +0000

    11:46:17.887 Running build in Washington, D.C., USA (East) – iad1 11:46:

--- src/app/api/genkit/route.ts
+++
@@ -1,5 +0,0 @@
-import {nextPlugin} from '@genkit-ai/next';
-
-// NOTE: This is required for Genkit to work with Next.js.
-// There is no need to edit this file.
-export const {POST} = nextPlugin();
