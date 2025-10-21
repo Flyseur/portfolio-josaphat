51a38caa7f9972189a8e317142a453a65a26edf3
Josaphat Boua <josaphatboua220@gmail.com>
2025-10-21 12:54:38 +0000

    sur le format web l'icone est l'icon de pc mais sur le format  mobile sa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,8 @@ export const metadata: Metadata = {
   description: 'Portfolio de Boua Josaphat Olivier Konan, un développeur full stack créatif.',
   icons: {
     icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💻</text></svg>',
+    apple: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💻</text></svg>',
+    shortcut: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💻</text></svg>',
   },
 };
 
